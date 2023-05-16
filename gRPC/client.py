b8d6a19a36eb9335c1430c743550c09e59bb100f
Rosialdo <79479462+Rosialdo@users.noreply.github.com>
2023-05-16 16:24:33 -0300

    Troca de todos os arquivos e adição do readme

--- gRPC/client.py
+++ gRPC/client.py
@@ -3,18 +3,25 @@ import click
 import remote_control_pb2
 import remote_control_pb2_grpc
 
-
 @click.command()
-@click.option('--host', default='localhost', help='Remote control server host.')
-@click.option('--port', default=50051, help='Remote control server port.')
-@click.argument('command', nargs=-1)
-def execute_command(host, port, command):
-    command_str = ' '.join(command)
-    with grpc.insecure_channel(f'{host}:{port}') as channel:
-        stub = remote_control_pb2_grpc.RemoteControlStub(channel)
-        response = stub.ExecuteCommand(remote_control_pb2.ExecuteCommandRequest(command=command_str))
+@click.option('--server', default='localhost:50051', help='endereço do servidor')
+def execute_command(server):
+    # Cria um canal de comunicação gRPC com o servidor especificado
+    channel = grpc.insecure_channel(server)
+    
+    # Cria um stub para chamar os métodos remotos do serviço
+    stub = remote_control_pb2_grpc.RemoteControlStub(channel)
+    
+    while True:
+        # Solicita ao usuário para digitar um comando
+        command = input('Digite o comando desejado: ')
+        
+        # Chama o método ExecuteCommand do serviço remoto, passando o comando como parâmetro
+        response = stub.ExecuteCommand(remote_control_pb2.CommandRequest(command=command))
+        
+        # Imprime a saída do comando
         print(response.output)
 
-
 if __name__ == '__main__':
+    # Executa a função execute_command quando o script é executado diretamente
     execute_command()
--- gRPC/server.py
+++ gRPC/server.py
@@ -1,26 +1,28 @@
-import grpc
 import subprocess
+import grpc
 import remote_control_pb2
 import remote_control_pb2_grpc
 from concurrent import futures
+import logging
 
+# Configuração básica do registro de log
+logging.basicConfig(filename='server.log', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
 
+# Implementação do serviço gRPC
 class RemoteControlServicer(remote_control_pb2_grpc.RemoteControlServicer):
     def ExecuteCommand(self, request, context):
-        try:
-            output = subprocess.check_output(request.command, shell=True)
-            return remote_control_pb2.ExecuteCommandResponse(output=output.decode())
-        except subprocess.CalledProcessError as e:
-            return remote_control_pb2.ExecuteCommandResponse(output=e.output.decode())
+        command = request.command
+        logging.info(f'Command received: {command}')  # Registra o comando recebido no log
+        output = subprocess.check_output(request.command, shell=True)  # Executa o comando no sistema operacional
+        return remote_control_pb2.CommandResponse(output=output.decode('utf-8'))  # Retorna a saída do comando
 
-
+# Função para iniciar o servidor gRPC
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    remote_control_pb2_grpc.add_RemoteControlServicer_to_server(RemoteControlServicer(), server)
-    server.add_insecure_port('[::]:50051')
-    server.start()
-    server.wait_for_termination()
-
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))  # Cria um servidor gRPC
+    remote_control_pb2_grpc.add_RemoteControlServicer_to_server(RemoteControlServicer(), server)  # Registra o serviço no servidor
+    server.add_insecure_port('[::]:50051')  # Configura a porta em que o servidor irá escutar as conexões
+    server.start()  # Inicia o servidor
+    server.wait_for_termination()  # Aguarda as chamadas de procedimento remoto
 
 if __name__ == '__main__':
-    serve()
+    serve()  # Inicia o servidor quando o script é executado diretamente
